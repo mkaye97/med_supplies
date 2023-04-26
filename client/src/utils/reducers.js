e39c925fc0ac1b17a3fc146c0bceb6231bfe75ba
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-26 12:58:12 -0400

    finishing up actions & reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,80 +3,140 @@
 //
 // 
 //
-import React, { createContext, useContext, useReducer } from 'react';
+import { useReducer } from 'react';
 
-const initialState = {
-    categories: [],
-    products: [],
-    selectedProduct: null,
-    cart: [],
-    user: null,
-    order: null,
-    checkout: null,
-    charities: [],
-    rentals: [],
-    donations: [],
-    filters: {
-        category: '',
-        minPrice: 0,
-        maxPrice: 0,
-      },
-    isAuthenticated: false,
-  };
+import {
+    UPDATE_PRODUCTS,
+    ADD_TO_CART,
+    UPDATE_CART_QUANTITY,
+    REMOVE_FROM_CART,
+    ADD_MULTIPLE_TO_CART,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY,
+    CLEAR_CART,
+    TOGGLE_CART,
+    SET_CATEGORIES,
+    SET_PRODUCTS,
+    SET_SELECTED_PRODUCT,
+    SET_USER,
+    SET_ORDER,
+    SET_CHECKOUT,
+    SET_CHARITIES,
+    SET_RENTALS,
+    SET_DONATIONS
+  } from "./actions";
+
+
   
-  const reducer = (state = initialState, action) => {
+  const reducer = (state, action) => {
     switch (action.type) {
-      case "SET_CATEGORIES":
+        case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map(product => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity
+          }
+          return product
+        })
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: []
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen
+      };
+
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory
+      };
+      case SET_CATEGORIES:
         return {
           ...state,
           categories: action.payload,
         };
-      case "SET_PRODUCTS":
+      case SET_PRODUCTS:
         return {
           ...state,
           products: action.payload,
         };
-      case "SET_SELECTED_PRODUCT":
+      case SET_SELECTED_PRODUCT:
         return {
           ...state,
           selectedProduct: action.payload,
         };
-      case "ADD_TO_CART":
-        return {
-          ...state,
-          cart: [...state.cart, action.payload],
-        };
-      case "REMOVE_FROM_CART":
-        return {
-          ...state,
-          cart: state.cart.filter((item) => item._id !== action.payload),
-        };
-      case "SET_USER":
+      case SET_USER:
         return {
           ...state,
           user: action.payload,
         };
-      case "SET_ORDER":
+      case SET_ORDER:
         return {
           ...state,
           order: action.payload,
         };
-      case "SET_CHECKOUT":
+      case SET_CHECKOUT:
         return {
           ...state,
           checkout: action.payload,
         };
-      case "SET_CHARITIES":
+      case SET_CHARITIES:
         return {
           ...state,
           charities: action.payload,
         };
-      case "SET_RENTALS":
+      case SET_RENTALS:
         return {
           ...state,
           rentals: action.payload,
         };
-      case "SET_DONATIONS":
+      case SET_DONATIONS:
         return {
           ...state,
           donations: action.payload,
