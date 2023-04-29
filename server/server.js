00a1f52e0def15a9ed772d5406b3d23e94f2f99c
mkaye <mattkaye97@gmail.com>
2023-04-28 22:16:02 -0400

    Re-added new server route

--- server/server.js
+++ server/server.js
@@ -30,6 +30,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
