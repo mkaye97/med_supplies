8e048c0956fe71e27d901a2a3d336d11f64c02d2
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-30 16:10:37 -0400

    renaming indexedDB from supply-cabinet to med-supplies

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -7,7 +7,7 @@ export function pluralize(name, count) {
 
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open('supply-cabinet', 1);
+    const request = window.indexedDB.open('med-supplies', 1);
     let db, tx, store;
     request.onupgradeneeded = function(e) {
       const db = request.result;
